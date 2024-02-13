ef7bb8dda06e5b896922aa9503d8a004cbd0ea96
GiuliaPaggi <giulia.paggi@studio.unibo.it>
2024-02-13 17:02:36 +0100

    Separate muon and shower events

--- digi_preprocess_list.py
+++ digi_preprocess_list.py
@@ -2,13 +2,32 @@ import ROOT
 import numpy as np
 import pandas as pd
 
+def writeFiles(toFile, opt) :
+   df = pd.DataFrame(data=toFile)
+   event_per_file = 10000
+   n_file = round(len(toFile)/event_per_file)
+   dfs = [0]*n_file
+   filenames = [0]*n_file
+   for n in range(n_file):
+      filenames[n]="../results/"+opt+"_DoubleMuon_"+str(n)+".csv"
+      if n == 0 : dfs[n]= df.iloc[:(event_per_file) ,:]
+      elif n == n_file : dfs[n]= df.iloc[(n*event_per_file):,:]
+      else : dfs[n]= df.iloc[((n)*event_per_file):((n+1)*event_per_file),:]
+
+   output_files = [open(str(files), 'w') for files in filenames]
+
+   for n in range(n_file):
+      dfs[n].to_csv(output_files[n])
+
 file_path = "/home/gpaggi/ConcentratorStudies/Ntuples/DoubleMuon_FlatPt-1To100_200PU_noRPC.root"
+#HTo2LongLivedTo4mu_MH-125_MFF-50_CTau-3000mm_noPU_noRPC.root"
 
 file = ROOT.TFile.Open(file_path)
 
 tree = file.Get("dtNtupleProducer/DTTREE")
 
 output = [] #[[0]*70]*4 #np.empty((4, 70), dtype=int)
+output_muons = []
 
 for i_event, event in enumerate(tree):
 
@@ -24,25 +43,12 @@ for i_event, event in enumerate(tree):
 
             digi_list[(event.digi_layer[i_digi]-1)*75+(event.digi_wire[i_digi]-1)] +=1
             
-      if max(digi_list) > 0 and digi_list.count(1)>5:
-         output.append(digi_list)
+      if max(digi_list) > 0 :
+         if digi_list.count(1) > 15:  output.append(digi_list)
+         else : output_muons.append(digi_list)
 
-
-df = pd.DataFrame(data=output)
-event_per_file = 10000
-n_file = round(len(output)/event_per_file)
-dfs = [0]*n_file
-filenames = [0]*n_file
-for n in range(n_file):
-   filenames[n]="../results/DoubleMuonDigiListOutput_"+str(n)+".csv"
-   if n == 0 : dfs[n]= df.iloc[:(event_per_file) ,:]
-   elif n == n_file : dfs[n]= df.iloc[(n*event_per_file):,:]
-   else : dfs[n]= df.iloc[((n)*event_per_file):((n+1)*event_per_file),:]
-
-output_files = [open(str(files), 'w') for files in filenames]
-
-for n in range(n_file):
-   dfs[n].to_csv(output_files[n])
+writeFiles(output, "shower")
+writeFiles(output_muons, "muon")
 
 print('\nDone')
 
